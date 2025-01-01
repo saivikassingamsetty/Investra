5633573e71f7f5b5ec6b9b71b73ec9e08df306d6
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-01-02 00:47:45 +0530

    Add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1 class="text-3xl font-bold mx-auto text-center my-10 text-blue-400">Hello World</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
