55e99293f43a89d0f3a60137e0c32913d1d26774
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-01-03 16:58:54 +0530

    Clear scaffolded files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header>
+    <h1 class="text-3xl font-bold mx-auto text-center my-10 text-blue-400">Hello World</h1>
+    Header
+  </header>
+  <main>Main Content</main>
+  <footer>Footer</footer>
 </template>
+
+<script setup lang="ts"></script>
