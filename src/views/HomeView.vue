25b181b7435f18d578de14f0e3971a1cfb11f8a6
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-01-04 00:35:13 +0530

    header part 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
-  <header id="header" class="sticky top-0 z-10 border-slate-500 border">
-    <section class="max-w-4xl mx-auto p-4 border-blue-500 border">
+  <header id="header" class="sticky top-0 z-10">
+    <section class="flex justify-between">
       <img
         src="../assets/investra_logo.png"
         alt="Investra Logo"
-        class="h-20 w-20 object-fill investra-header-logo"
+        class="h-20 w-20"
+        draggable="false"
       />
+      <nav class="m-6">
+        <a class="bg-blue-200 p-2 rounded hover:bg-blue-300 cursor-pointer">Login</a>
+      </nav>
     </section>
   </header>
-  <section>Hi</section>
-  <main>Main Content</main>
-  <footer>Footer</footer>
 </template>
 
 <script setup lang="ts"></script>
