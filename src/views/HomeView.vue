a4492f39943d326004658b7f4930c819f5dbb047
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-01-03 17:34:24 +0530

    initial changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
-  <header>
-    <h1 class="text-3xl font-bold mx-auto text-center my-10 text-blue-400">Hello World</h1>
-    Header
+  <header id="header" class="sticky top-0 z-10 border-slate-500 border">
+    <section class="max-w-4xl mx-auto p-4 border-blue-500 border">
+      <img
+        src="../assets/investra_logo.png"
+        alt="Investra Logo"
+        class="h-20 w-20 object-fill investra-header-logo"
+      />
+    </section>
   </header>
+  <section>Hi</section>
   <main>Main Content</main>
   <footer>Footer</footer>
 </template>
