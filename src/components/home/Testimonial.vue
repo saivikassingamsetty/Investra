36659cab65978136b1db920b0b0e3eabd96cc972
Sai Vikas Singamsetty <saivikas.singamsetty@gmail.com>
2025-01-05 01:53:27 +0530

    add testimonial images

--- src/components/home/Testimonial.vue
+++ src/components/home/Testimonial.vue
@@ -2,7 +2,7 @@
   <div class="bg-white m-6 p-6 flex flex-col gap-4 shadow-lg rounded-lg">
     <!-- PP and Name -->
     <div class="flex gap-4">
-      <img :src="pic" alt="Image" class="rounded-full" />
+      <img :src="pic" alt="Image" height="40" width="60" class="rounded-full" />
       <div class="text-center">
         <h1 class="text-xl font-semibold">{{ name }}</h1>
         <p class="font-small text-slate-600">{{ role }}</p>
