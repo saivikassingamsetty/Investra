ae2aeafbb00dc64142ec8cb1f52e48d530903df1
Sai Vikas Singamsetty <112643982+saivikassingamsetty@users.noreply.github.com>
2025-02-26 01:04:20 +0530

    feat(portfolio-overview): implement portfolio overview tiles (#34)

--- src/components/dashboard/DashboardContent.vue
+++ src/components/dashboard/DashboardContent.vue
@@ -1,77 +1,32 @@
 <template>
-  <div class="flex flex-wrap">
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 'small' }}
+  <div class="grid grid-cols-12 gap-4 p-6">
+    <!-- Portfolio Summary -->
+    <div class="col-span-12 bg-white p-6 rounded-xl shadow-lg">
+      <PortfolioSummary />
     </div>
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 'small' }}
+    <!-- Portfolio Allocation -->
+    <div class="col-span-6 bg-white p-6 rounded-xl shadow-lg">
+      <PortfolioAllocation />
     </div>
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 'small' }}
+    <!-- Portfolio Performance -->
+    <div class="col-span-6 bg-white p-6 rounded-xl shadow-lg">
+      <PortfolioPerformance />
     </div>
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 'small' }}
+    <!-- Gainers / Losers -->
+    <div class="col-span-4 bg-white p-6 rounded-xl shadow-lg">
+      <GainersLosers />
     </div>
-    <div
-      class="flex-grow basis-full h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 'big' }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-
-    <div
-      class="flex-grow basis-96 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-52 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
-    </div>
-    <div
-      class="flex-grow basis-80 h-52 bg-blue-200 m-5 rounded-lg flex justify-center items-center"
-    >
-      {{ 1 }}
+    <!-- Recent Transactions -->
+    <div class="col-span-8 bg-white p-6 rounded-xl shadow-lg">
+      <RecentTransactions />
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PortfolioSummary from './portfolio-overview/PortfolioSummary.vue'
+import PortfolioAllocation from './portfolio-overview/PortfolioAllocation.vue'
+import PortfolioPerformance from './portfolio-overview/PortfolioPerformance.vue'
+import GainersLosers from './portfolio-overview/GainersLosers.vue'
+import RecentTransactions from './portfolio-overview/RecentTransactions.vue'
+</script>
---
+++ src/components/dashboard/portfolio-overview/RecentTransactions.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold text-gray-800 mb-4">Recent Transactions</h2>
+    <!-- Recent Transactions Table -->
+    <table class="w-full text-left mt-4">
+      <thead>
+        <tr class="border-b">
+          <th class="p-2">Type</th>
+          <th class="p-2">Stock</th>
+          <th class="p-2">Qty</th>
+          <th class="p-2">Price</th>
+          <th class="p-2">Total</th>
+          <th class="p-2">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="{ id, stock, qty, price, total, date, type } in transactions"
+          :key="id"
+          class="border-b"
+        >
+          <td class="p-2" :class="type == 'BUY' ? 'text-green-600' : 'text-red-600'">{{ type }}</td>
+          <td class="p-2">{{ stock }}</td>
+          <td class="p-2">{{ qty }}</td>
+          <td class="p-2">{{ price }}</td>
+          <td class="p-2">{{ total }}</td>
+          <td class="p-2">{{ date }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const transactions = ref([
+  {
+    id: 1,
+    type: 'BUY',
+    stock: 'Marksans',
+    qty: 120,
+    price: 400,
+    total: 48000,
+    date: '26 Feb 2025',
+  },
+  { id: 1, type: 'BUY', stock: 'Skipper', qty: 10, price: 1200, total: 12000, date: '26 Feb 2025' },
+  { id: 1, type: 'BUY', stock: 'Exicom', qty: 10, price: 600, total: 6000, date: '26 Feb 2025' },
+  {
+    id: 1,
+    type: 'SELL',
+    stock: 'Infibeam',
+    qty: 1000,
+    price: 20,
+    total: 20000,
+    date: '26 Feb 2025',
+  },
+])
+</script>
