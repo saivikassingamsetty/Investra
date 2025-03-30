98ce284f7c52359fd5fea733370cf39d48c8bb74
saivikassingamsetty <saivikas.singamsetty@gmail.com>
2025-03-30 20:31:38 +0530

    refactor(codebase): move to client/server architecture

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -10,7 +9,7 @@ export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './'),
     },
   },
 })
